202361c23b92f7ef1d2dc6a0d3bbd6b9b7e1b2a8
Lucca Bessa <32376315+LuccaBessa@users.noreply.github.com>
2023-06-06 20:15:30 -0300

    Gerar extrato em PDF

--- implementacao/front/src/features/Home/pages/Extrato.component.tsx
+++ implementacao/front/src/features/Home/pages/Extrato.component.tsx
@@ -2,6 +2,8 @@ import { HomeService } from '../services';
 import { ISaldo } from '../services/interfaces';
 import { useQuery } from '@tanstack/react-query';
 import { TransacaoForm } from '../components';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 const modalId: string = 'transacaoForm';
 
@@ -23,6 +25,28 @@ export const Extrato = () => {
     refetchOnWindowFocus: false,
   });
 
+  const onClickGenerateReport = async (): Promise<void> => {
+    const doc = new jsPDF();
+
+    const lineY = 20;
+    const date = new Date().toLocaleDateString();
+
+    doc.setLineWidth(0.5);
+    doc.setFontSize(14);
+    doc.text(`Saldo: ${data?.moedas}`, 15, lineY);
+    doc.setFontSize(10);
+    doc.text(`Gerado em: ${date}`, doc.internal.pageSize.getWidth() - 15 - doc.getStringUnitWidth(date) * doc.getFontSize(), lineY);
+
+    autoTable(doc, {
+      startY: 28,
+      headStyles: { fillColor: '#661ae6' },
+      head: [['Tipo', 'Remetente', 'Destinatário', 'Valor', 'Descrição']],
+      body: data?.transacoes.map((transacao) => [transacao.remetenteId !== id ? 'Recebimento' : 'Envio', id == transacao.remetenteId ? '-' : transacao.remetenteNome, id == transacao.destinatarioId ? '-' : transacao.destinatarioNome, transacao.valor, transacao.descricao]),
+    });
+
+    doc.save('extrato.pdf');
+  };
+
   return (
     <>
       <div className='flex flex-col grow h-full'>
@@ -33,11 +57,16 @@ export const Extrato = () => {
               <path strokeLinecap='round' strokeLinejoin='round' d='M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
             </svg>
           </div>
-          {tipo === 'professor' && (
-            <label htmlFor={modalId} className='btn btn-primary shadow'>
-              Nova Transação
-            </label>
-          )}
+          <div className='flex gap-2'>
+            <button className='btn btn-primary shadow' onClick={() => onClickGenerateReport()}>
+              Gerar Relatório
+            </button>
+            {tipo === 'professor' && (
+              <label htmlFor={modalId} className='btn btn-primary shadow'>
+                Nova Transação
+              </label>
+            )}
+          </div>
         </div>
         <div className='flex flex-col mx-12 my-6 h-full bg-base-200 rounded-md shadow-lg'>
           <table className='table w-full'>
